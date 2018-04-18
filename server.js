3cf63dd9fe7b8102f0dce8bf78e51cf8a68dd7d4
Joey Dye <joey.m.dye@gmail.com>
2018-04-18 06:54:24 -0500

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-corrugated-27102',
     user: 'joeydye',
     password: '',
     database: 'smart-brain'
