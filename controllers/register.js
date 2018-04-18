49e50e45bb61071f5fe1d1df65ceb30664be399c
Joey Dye <joey.m.dye@gmail.com>
2018-04-17 19:45:37 -0500

    testing

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json('Incorrect form submission');
+    return res.status(400).json('incorrect form submission');
   }
   // Create hash
   const hash = bcrypt.hashSync(password);
